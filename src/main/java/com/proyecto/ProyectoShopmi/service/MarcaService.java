f65120b105d2268a2ad1ebbf3bb39087212ec01e
Yari09210 <Yari.ha.09210@gmail.com>
2025-04-09 19:15:58 -0500

    Update MarcaService.java

--- src/main/java/com/proyecto/ProyectoShopmi/service/MarcaService.java
+++ src/main/java/com/proyecto/ProyectoShopmi/service/MarcaService.java
@@ -1,1 +1,33 @@
+package com.proyecto.ProyectoShopmi.service;
+
+import com.proyecto.ProyectoShopmi.entity.Marca;
+import com.proyecto.ProyectoShopmi.entity.Usuario;
+import com.proyecto.ProyectoShopmi.repository.MarcaRepository;
+import org.hibernate.dialect.lock.OptimisticEntityLockException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MarcaService {
+    @Autowired
+    MarcaRepository repositorio;
+
+    public List<Marca> getMarcas() {
+        return repositorio.findAll();
+    }
+
+    public Optional<Marca> getMarcas(Long id)
+    {
+        return repositorio.findById(id);
+    }
+
+    public void saveUpdate(Marca marca)
+    {
+        repositorio.save(marca);
+    }
+
+}
 
